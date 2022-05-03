5d48e924c71d83bdd9f8e7f9a3e5f93dca1a41ac
Andrew Bradt <andrewjbradt@gmail.com>
2022-05-03 13:56:07 -0400

    replace radio labels with normal labels, add click events

--- native-application/src/index.js
+++ native-application/src/index.js
@@ -1,5 +1,9 @@
 const { app, BrowserWindow, Tray, Menu, nativeImage } = require('electron');
 const path = require('path');
+
+// Helper Functions/Variables
+const trashBinPath = require('./helpers/trash-bin-path');
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   // eslint-disable-line global-require
@@ -9,10 +13,12 @@ if (require('electron-squirrel-startup')) {
 const createTray = () => {
   const tray = new Tray(path.join(__dirname, 'assets', 'icon_tray.png'));
   const contextMenu = Menu.buildFromTemplate([
-    {label: 'Item1', type:'radio'},
-    {label: 'Item1', type:'radio', checked: true}
+    {label: 'Run', click: () => console.log('clicked run')},
+    {label: 'Quit', click: () => console.log('clicked quit')}
   ]);
   tray.setContextMenu(contextMenu);
+
+
 };
 
 app.on('ready', createTray);
