8624948c740acca8f590bcb8696c57722fc78db4
Andrew Bradt <andrewjbradt@gmail.com>
2022-05-03 15:19:47 -0400

    modify watch-dir-for-changes, returns  beginWatch and abortWatch funcs

--- native-application/src/helpers/watch-dir-for-changes.js
+++ native-application/src/helpers/watch-dir-for-changes.js
@@ -1,14 +1,18 @@
 const {watch} = require('fs/promises');
-const ac = new AbortController();
-const {signal} = ac;
-const abortWatchDir = () => ac.abort();
 
-const watchForNewFileOfType = (path, regexp) => async() => {
-  const watcher = watch(path, {signal, recursive: true});
+module.exports = (path, regexp) => () => {
+  const ac = new AbortController();
+  const {signal} = ac;
 
-  for await (const event of watcher) {
-    console.log(event);
-  }
+  const abortWatch = () => ac.abort();
+  
+  const beginWatch = async() => {
+    const watcher = watch(path, {signal, recursive: true});
+
+    for await (const event of watcher) {
+      console.log(event);
+    }
+  };
+  
+  return {beginWatch, abortWatch};
 };
-
-module.exports = {watchForNewFileOfType, abortWatchDir};
