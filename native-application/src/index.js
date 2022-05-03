1c091ed8496342d1a1e796f3570b7272a865ffcd
Andrew Bradt <andrewjbradt@gmail.com>
2022-05-03 13:42:10 -0400

    remove boilerplate code for window creation, create basic tray with image

--- native-application/src/index.js
+++ native-application/src/index.js
@@ -1,30 +1,21 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Tray, Menu, nativeImage } = require('electron');
 const path = require('path');
-
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   // eslint-disable-line global-require
   app.quit();
 }
 
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-  });
-
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+const createTray = () => {
+  const tray = new Tray(path.join(__dirname, 'assets', 'icon_tray.png'));
+  const contextMenu = Menu.buildFromTemplate([
+    {label: 'Item1', type:'radio'},
+    {label: 'Item1', type:'radio', checked: true}
+  ]);
+  tray.setContextMenu(contextMenu);
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', createTray);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -42,6 +33,3 @@ app.on('activate', () => {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
