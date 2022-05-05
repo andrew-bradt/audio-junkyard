1d26a553fe9c90942fe7fa96499cccf0d1cb34bc
Andrew Bradt <andrewjbradt@gmail.com>
2022-05-05 11:14:12 -0400

    add upload route

--- application_server/app.js
+++ application_server/app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
+const uploadRouter = require('./routes/upload');
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/upload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
