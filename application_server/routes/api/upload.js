04365e6ab062fae6ff9eec582f10615641645fff
Andrew Bradt <andrewjbradt@gmail.com>
2022-05-10 20:56:35 -0400

    implement upload route with multer

--- application_server/routes/api/upload.js
+++ application_server/routes/api/upload.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 
-router.post('/', (req, res) => {
+const storage = multer.diskStorage({
+  filename: (req, file, cb) => {
+    const name = `${Date.now()}_${file.originalname}`;
+    cb(null, name);
+  },
+  destination: (req, file, cb) => {
+    cb(null, './uploads');
+  },
+});
+
+const upload = multer({storage});
+
+router.post('/', upload.array('audio'), (req, res) => {
   res.send('api/upload');
 });
 
